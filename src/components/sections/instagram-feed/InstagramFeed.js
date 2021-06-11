0600e27fb8fbdad5cf8df922ddc4760204ce863c
Marek Matejovic <me@devmaroy.com>
2021-06-11 13:49:01 +0200

    [MW-16] Instagram Feed - mobile

---
+++ src/components/sections/instagram-feed/InstagramFeed.js
@@ -0,0 +1,65 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+import { GatsbyImage } from 'gatsby-plugin-image';
+import Slider from 'react-slick';
+import SectionHeader from '../../common/SectionHeader';
+
+// API Query
+const APIQuery = graphql`
+  query SectionInstagramFeedQuery {
+    allFile(
+      filter: {
+        sourceInstanceName: { eq: "images" }
+        relativeDirectory: { eq: "sections/instagram-feed" }
+      }
+      sort: { fields: base, order: ASC }
+    ) {
+      nodes {
+        id
+        childImageSharp {
+          gatsbyImageData(width: 1200, quality: 100)
+        }
+      }
+    }
+  }
+`;
+
+const InstagramFeed = () => {
+  const data = useStaticQuery(APIQuery);
+  const APISectionInstagramFeedData = data.allFile.nodes;
+
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+  };
+
+  return (
+    <section className="section instagram-feed">
+      <div className="container">
+        <div className="section__inner instagram-feed__inner">
+          <SectionHeader heading="Follow Me" subHeading="Instagram" />
+
+          <div className="section__content">
+            <Slider {...sliderSettings}>
+              {APISectionInstagramFeedData.map(({ id, childImageSharp }) => (
+                <div key={id} className="instagram-feed__item">
+                  <GatsbyImage
+                    image={childImageSharp.gatsbyImageData}
+                    alt="Portfolio image"
+                  />
+                </div>
+              ))}
+            </Slider>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default InstagramFeed;
