ee007590c1080dfbc19b3fa842144450daf36e0f
Marek Matejovic <me@devmaroy.com>
2021-06-17 16:36:22 +0200

    [MW-25] Blog post implemented - mobile

--- src/types/components/common/social/socialInnerType.js
+++ src/types/components/common/social/socialInnerType.js
@@ -2,7 +2,7 @@ import { shape, string } from 'prop-types';
 
 // Social Inner Type
 export default shape({
-  type: string.isRequired,
+  type: string,
   url: string.isRequired,
   name: string.isRequired,
 });
