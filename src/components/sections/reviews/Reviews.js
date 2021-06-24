29ee1cb4695c318bb3b96b5f95d8838e61d2885b
Marek Matejovic <me@devmaroy.com>
2021-06-24 19:03:15 +0200

    [MW-45] Reviews now uses WP API

--- src/components/sections/reviews/Reviews.js
+++ src/components/sections/reviews/Reviews.js
@@ -2,22 +2,30 @@ import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 import SectionHeader from '../../common/SectionHeader';
 import Card from '../../common/Card';
-import ratingIcon from '../../../images/icons/rating.svg';
 
 // API Query
 const APIQuery = graphql`
   query SectionReviewsQuery {
-    allFile(
-      filter: {
-        sourceInstanceName: { eq: "images" }
-        relativeDirectory: { eq: "sections/reviews" }
-      }
-      sort: { fields: base, order: ASC }
-    ) {
-      nodes {
-        id
-        childImageSharp {
-          gatsbyImageData(width: 40, quality: 100)
+    wpgraphql {
+      sectionReviews {
+        nodes {
+          id
+          acf {
+            text
+            icon {
+              altText
+              sourceUrl
+            }
+            author {
+              image {
+                altText
+                sourceUrl
+              }
+              name
+              company
+              position
+            }
+          }
         }
       }
     }
@@ -26,44 +34,7 @@ const APIQuery = graphql`
 
 const Reviews = () => {
   const data = useStaticQuery(APIQuery);
-  const APISectionReviewsData = data.allFile.nodes;
-
-  const cards = [
-    {
-      id: '2fc71fb2-45ee-425b-9a3b-241c107b98e9',
-      icon: ratingIcon,
-      text: `<p>“Bonbon cheesecake powder cakes chocolate pudding chocolate 
-      candy canes marzipan gummi bears jelly pie.“</p>`,
-      author: {
-        image: APISectionReviewsData[0],
-        name: 'William Soule',
-        position: `Manager at <strong>Tiramisu, Inc</strong>`,
-      },
-    },
-    {
-      id: '24bb1e97-6ac6-4889-b4e0-60d173cf48ab',
-      icon: ratingIcon,
-      text: `<p>“Chocolate chupa chups gummies pastry dessert. 
-      Cake marshmallow chocolate bar toffee chocolate bar wafer chocolate 
-      cake.”</p>`,
-      author: {
-        image: APISectionReviewsData[1],
-        name: 'Daniella Jones',
-        position: `Consultant at <strong>DJones, Inc</strong>`,
-      },
-    },
-    {
-      id: '790f5d92-cdcc-4c1a-8350-fea512f78ff3',
-      icon: ratingIcon,
-      text: `<p>“Lemon drops cake candy halvah candy canes. Dessert croissant 
-      ice cream cake apple pie icing. Candy canes danish canes biscuit.“ </p>`,
-      author: {
-        image: APISectionReviewsData[2],
-        name: 'Hope Reese',
-        position: 'Freelance developer',
-      },
-    },
-  ];
+  const APISectionReviewsData = data.wpgraphql.sectionReviews.nodes;
 
   return (
     <section id="reviews" className="section reviews">
@@ -73,15 +44,18 @@ const Reviews = () => {
 
           <div className="section__content">
             <div className="card__responsive-layout reviews__cards">
-              {cards.map(({ id, icon, text, author }) => (
-                <Card
-                  key={id}
-                  icon={icon}
-                  iconSize="lg"
-                  text={text}
-                  author={author}
-                />
-              ))}
+              {APISectionReviewsData.map(
+                ({ id, acf }) =>
+                  console.log(acf) || (
+                    <Card
+                      key={id}
+                      icon={acf.icon}
+                      iconSize="lg"
+                      text={acf.text}
+                      author={acf.author}
+                    />
+                  ),
+              )}
             </div>
           </div>
         </div>
