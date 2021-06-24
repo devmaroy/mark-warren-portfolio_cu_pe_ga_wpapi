d43bdb4d4c0381675615d7abb84acd48d3e7a5dc
Marek Matejovic <me@devmaroy.com>
2021-06-24 15:27:03 +0200

    [MW-41] About now uses WP API

--- src/types/components/sections/about/aboutInfoType.js
+++ src/types/components/sections/about/aboutInfoType.js
@@ -3,7 +3,12 @@ import { string, arrayOf, shape } from 'prop-types';
 // About Info Type
 export default {
   text: string.isRequired,
-  technologies: arrayOf(
-    shape({ id: string.isRequired, name: string.isRequired }).isRequired,
-  ).isRequired,
+  technologies: shape({
+    heading: string.isRequired,
+    list: arrayOf(
+      shape({
+        name: string.isRequired,
+      }).isRequired,
+    ).isRequired,
+  }),
 };
