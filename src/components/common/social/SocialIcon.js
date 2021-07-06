f4c43de581cfb0e24d5a3c92d786a68ee39e9991
Marek Matejovic <me@devmaroy.com>
2021-07-06 18:41:00 +0200

    [MW-70] Fixed social icons and added more variants

--- src/components/common/social/SocialIcon.js
+++ src/components/common/social/SocialIcon.js
@@ -3,12 +3,29 @@ import classNames from 'classnames';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import socialIconType from '../../../types/components/common/social/socialIconType';
 import kofiIcon from '../../../images/icons/social/kofi.svg';
+import mailruIcon from '../../../images/icons/social/mailru.svg';
+import mailruSquareIcon from '../../../images/icons/social/mailru-square.svg';
+import livejournalIcon from '../../../images/icons/social/livejournal.svg';
+import livejournalSquareIcon from '../../../images/icons/social/livejournal-square.svg';
+import workplaceIcon from '../../../images/icons/social/workplace.svg';
+import instapaperIcon from '../../../images/icons/social/instapaper.svg';
+import instapaperSquareIcon from '../../../images/icons/social/instapaper-square.svg';
+import hatenaIcon from '../../../images/icons/social/hatena.svg';
+import hatenaSquareIcon from '../../../images/icons/social/hatena-square.svg';
+import emailIcon from '../../../images/icons/social/email.svg';
+import emailSquareIcon from '../../../images/icons/social/email-square.svg';
 
-const SocialIcon = ({ icon, type = 'normal', fixedWidth = true }) => {
+const SocialIcon = ({
+  icon,
+  type = 'normal',
+  fixedWidth = true,
+  className = 'social-icon__img',
+}) => {
   const socialIcon = icon.toLowerCase();
 
   const squareIcons = {
     facebook: 'facebook-square',
+    messenger: 'facebook-messenger',
     twitter: 'twitter-square',
     linkedin: 'linkedin',
     youtube: 'youtube-square',
@@ -35,11 +52,21 @@ const SocialIcon = ({ icon, type = 'normal', fixedWidth = true }) => {
     github: 'github-square',
     trello: 'trello',
     patreon: 'patreon',
+    pocket: 'get-pocket',
+    weibo: 'weibo',
+    line: 'line',
     kofi: kofiIcon,
+    mailru: mailruSquareIcon,
+    livejournal: livejournalSquareIcon,
+    workplace: workplaceIcon,
+    instapaper: instapaperSquareIcon,
+    hatena: hatenaSquareIcon,
+    email: emailSquareIcon,
   };
 
   const normalIcons = {
     facebook: 'facebook-f',
+    messenger: 'facebook-messenger',
     twitter: 'twitter',
     linkedin: 'linkedin-in',
     youtube: 'youtube',
@@ -66,21 +93,42 @@ const SocialIcon = ({ icon, type = 'normal', fixedWidth = true }) => {
     github: 'github',
     trello: 'trello',
     patreon: 'patreon',
+    pocket: 'get-pocket',
+    weibo: 'weibo',
+    line: 'line',
     kofi: kofiIcon,
+    mailru: mailruIcon,
+    livejournal: livejournalIcon,
+    workplace: workplaceIcon,
+    instapaper: instapaperIcon,
+    hatena: hatenaIcon,
+    email: emailIcon,
   };
 
+  const imgIcons = [
+    'kofi',
+    'mailru',
+    'livejournal',
+    'workplace',
+    'instapaper',
+    'hatena',
+    'email',
+  ];
+
   const renderIcon = (typeToRender) => {
-    const imgIcons = ['kofi'];
+    const isImgIcon = imgIcons.includes(socialIcon);
+    const iconClassName = classNames(`svg-inline--fa ${className}`, {
+      'fa-fw': fixedWidth,
+    });
 
     switch (typeToRender) {
       case 'square':
-        return imgIcons.includes(socialIcon) ? (
-          <img
-            src={squareIcons[socialIcon]}
-            alt={socialIcon}
-            className={classNames('svg-inline--fa', { 'fa-fw': fixedWidth })}
-          />
-        ) : (
+        if (isImgIcon) {
+          const SocialIconComponent = squareIcons[socialIcon];
+          return <SocialIconComponent className={iconClassName} />;
+        }
+
+        return (
           <FontAwesomeIcon
             icon={['fab', squareIcons[socialIcon]]}
             fixedWidth={fixedWidth}
@@ -88,13 +136,12 @@ const SocialIcon = ({ icon, type = 'normal', fixedWidth = true }) => {
         );
 
       default:
-        return imgIcons.includes(socialIcon) ? (
-          <img
-            src={normalIcons[socialIcon]}
-            alt={socialIcon}
-            className={classNames('svg-inline--fa', { 'fa-fw': fixedWidth })}
-          />
-        ) : (
+        if (isImgIcon) {
+          const SocialIconComponent = normalIcons[socialIcon];
+          return <SocialIconComponent className={iconClassName} />;
+        }
+
+        return (
           <FontAwesomeIcon
             icon={['fab', normalIcons[socialIcon]]}
             fixedWidth={fixedWidth}
