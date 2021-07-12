375c159f0e25747cb386e8fb9b7ee94644acb0f2
Marek Matejovic <me@devmaroy.com>
2021-07-12 15:46:12 +0200

    [MW-72] Fixed path for post tags

--- src/components/pages/blog/post/BlogPostTags.js
+++ src/components/pages/blog/post/BlogPostTags.js
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GlobalContext } from '../../../../context/GlobalContextProvider';
 import Button from '../../../common/Button';
 import blogPostTagsType from '../../../../types/components/pages/blog/post/blogPostTagsType';
 
-const BlogPostTags = ({ tags }) => (
-  <ul className="blog-post-tags">
-    {tags.map(({ id, name, slug }) => (
-      <li key={id} className="blog-post-tags__tag">
-        <Button
-          to={slug}
-          variant="tertiary"
-          size="sm"
-          showIcon={false}
-          className="blog-post-tags__tag-link"
-        >
-          {name}
-        </Button>
-      </li>
-    ))}
-  </ul>
-);
+const BlogPostTags = ({ tags }) => {
+  const globalContextData = useContext(GlobalContext);
+  const { slug: blogTagSlug } = globalContextData.pages.blogTags;
+
+  return (
+    <ul className="blog-post-tags">
+      {tags.map(({ id, name, slug }) => (
+        <li key={id} className="blog-post-tags__tag">
+          <Button
+            to={`/${blogTagSlug}/${slug}`}
+            variant="tertiary"
+            size="sm"
+            showIcon={false}
+            className="blog-post-tags__tag-link"
+          >
+            {name}
+          </Button>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 BlogPostTags.propTypes = {
   ...blogPostTagsType,
