30b6b3bb4bea0cbad6afc2d4fe93d24a9e2f7e38
Marek Matejovic <me@devmaroy.com>
2021-07-23 20:10:01 +0200

    Fixed shapes

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,8 @@
 if (process.env.NODE_ENV === 'development') {
   process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 }
+// REMOVE
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 const path = require('path');
 const slugify = require('slugify');
